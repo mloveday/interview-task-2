34adcb3e3648613379d1044f017b5575cf7ade6d
miles <miles.loveday@gmail.com>
2019-06-24 21:05:29 +0100

    Redis basic usage

--- symfony/src/Controller/IndexController.php
+++ symfony/src/Controller/IndexController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Predis\Client as RedisClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController
 {
+    const KEY = 'test-key';
+    const TEST_DATA = 'Hello world!';
+    const TTL = 5;
+
     /** @Route("/", name="index") */
-    public function index()
+    public function index(RedisClient $redisClient)
     {
-        return new Response('Hello world!');
+        if ($redisClient->exists(self::KEY) === 0) {
+            $redisClient->setex(self::KEY, self::TTL, self::TEST_DATA);
+            $value = $redisClient->get(self::KEY);
+            return new Response($value . " (set)");
+        }
+        $value = $redisClient->get(self::KEY);
+        return new Response($value . " (cached)");
     }
 }
