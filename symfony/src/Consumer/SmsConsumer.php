e56561c9b1837a920487b821a2833dee914709bb
miles <miles.loveday@gmail.com>
2019-06-25 20:39:38 +0100

    Integrate with Twilio for sending SMS messages Phone numbers, settings currently hard-coded in .env.local (TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_SENDER_NUMBER, MY_PHONE_NUMBER)

--- symfony/src/Consumer/SmsConsumer.php
+++ symfony/src/Consumer/SmsConsumer.php
@@ -2,15 +2,33 @@
 
 namespace App\Consumer;
 
+use App\Service\TwilioClient;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 
 class SmsConsumer implements ConsumerInterface
 {
+    /** @var TwilioClient */
+    private $client;
+    /** @var string */
+    private $senderNumber;
+
+    public function __construct(TwilioClient $client)
+    {
+        $this->client = $client;
+        $this->senderNumber = $_ENV['TWILIO_SENDER_NUMBER'];
+    }
+
     public function execute(AMQPMessage $msg)
     {
-        // TODO this needs to handle the (as yet) unsent message, i.e. send via twilio
         echo $msg->getBody();
+        $this->client->messages->create(
+            $_ENV['MY_PHONE_NUMBER'], // TODO replace with number from message
+            [
+                'from' => $this->senderNumber,
+                'body' => $msg->getBody(),
+            ]
+        );
     }
 
 }
